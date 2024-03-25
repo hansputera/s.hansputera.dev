e20b0862dfd7e75f425d2000758a2483ee530359
Hanif Dwy Putra S <hanifdwyputrasembiring@gmail.com>
2024-03-25 15:00:28 +0000

    fix(api.deletion.[route]#types): fix types for Request parameter
    Signed-off-by: Hanif Dwy Putra S <hanifdwyputrasembiring@gmail.com>

--- src/app/api/deletion/[key]/route.ts
+++ src/app/api/deletion/[key]/route.ts
@@ -1,7 +1,7 @@
 import { deleteUrl } from "@/services/deleteUrl";
-import { NextApiRequest } from "next";
+import { NextRequest } from "next/server";
 
-export async function POST(_request: NextApiRequest, { params }: { params: { key: string } }) {
+export async function POST(_request: NextRequest, { params }: { params: { key: string } }) {
     // Get the key from the params
     const { key } = params;
 
