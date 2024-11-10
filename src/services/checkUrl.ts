d204d46c872b726cbfe48bcf4d7e0519a8c193f2
Hanif Dwy Putra S <hanifdwyputrasembiring@gmail.com>
2024-11-10 12:37:12 +0800

    feat(services.checkUrl): added timeout check

--- src/services/checkUrl.ts
+++ src/services/checkUrl.ts
@@ -1,12 +1,22 @@
 export const checkUrlService = async (url: string): Promise<boolean> => {
     try {
-        const response = await fetch(url, {
+        await fetch(url, {
             headers: {
                 'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.36',
-            }
+            },
+            signal: AbortSignal.timeout(5000),
         });
-        return response.ok;
-    } catch {
+
+        return true;
+    } catch (error) {
+        if (error instanceof Error) {
+            if (error.name === 'TimeoutError') {
+                return false;
+            } else {
+                return true;
+            }
+        }
+
         return false;
     }
 }
